fac958cec0bf6c4ede74651128b49f9201c09207
shellys172004-ui <shellys172004@gmail.com>
2026-01-22 21:57:22 +0530

    Update Sprite.js

--- js/Sprite.js
+++ js/Sprite.js
@@ -4,26 +4,12 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 class Sprite {
-    constructor({
-        position,
-        imageSrc,
-        scale = 1,
-        maxFrames = 1,
-        holdFrames = 30,
-        offsetFrame = { x: 0, y: 0 }
-    }) {
+    constructor({ position, imageSrc, scale = 1, maxFrames = 1, holdFrames = 30, offsetFrame = { x: 0, y: 0 } }) {
         this.position = position
-        this.height = 150
         this.width = 50
+        this.height = 150
 
         this.image = new Image()
-        this.loaded = false
-
-        // IMPORTANT: wait for image to load
-        this.image.onload = () => {
-            this.loaded = true
-        }
-
         this.image.src = imageSrc
 
         this.scale = scale
@@ -35,8 +21,7 @@ class Sprite {
     }
 
     draw() {
-        // ⛔ prevent drawImage crash if image not loaded yet
-        if (!this.loaded) return
+        if (!this.image.complete) return
 
         c.drawImage(
             this.image,
@@ -53,19 +38,9 @@ class Sprite {
 
     animateFrames() {
         this.elapsedFrames++
-
         if (this.elapsedFrames % this.holdFrames === 0) {
-            if (this.currentFrame < this.maxFrames - 1) {
-                this.currentFrame++
-            } else {
-                if (this instanceof Fighter) {
-                    if (this.health > 0) {
-                        this.currentFrame = 0
-                    }
-                } else {
-                    this.currentFrame = 0
-                }
-            }
+            this.currentFrame =
+                this.currentFrame < this.maxFrames - 1 ? this.currentFrame + 1 : 0
         }
     }
 
@@ -77,12 +52,12 @@ class Sprite {
 
 export const background = new Sprite({
     position: { x: 0, y: 0 },
-    imageSrc: '../assets/img/background.png'
+    imageSrc: '/dangerous_game/assets/img/background.png'
 })
 
 export const shop = new Sprite({
-    position: { x: 620, y: 128 },
-    imageSrc: '../assets/img/shop.png',
+    position: { x: 600, y: 128 },
+    imageSrc: '/dangerous_game/assets/img/shop.png',
     scale: 2.75,
     maxFrames: 6,
     holdFrames: 9
